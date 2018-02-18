d05db49f02eec3a6b4d762e45fb0a85e9333da0a
Chlece Neal <cneal2@luc.edu>
2018-02-18 13:26:24 -0600

    everything working
    Models work
    veiws work
    connection to db works
    link from courses does not work

--- PLI/PLCert/models.py
+++ PLI/PLCert/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
@@ -21,7 +23,7 @@ class Course(models.Model):
     #author = models.ForeignKey(User, on_delete=models.CASCADE, default='None')
     #subject = models.ForeignKey(Subject,  on_delete=models.CASCADE, default='None' )
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, default='None')
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
@@ -38,3 +40,34 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+class Content(models.Model):
+    module = models.ForeignKey(Module,on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, limit_choices_to={'model__in':('text','video','image','file')}, on_delete=models.CASCADE) 
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')    
+
+
+class ItemBase(models.Model):
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+       file = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
--- PLI/PLCert/views.py
+++ PLI/PLCert/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
+from .models import Module
+
 # Create your views here.
 def index(request):
     return render(request, 'PLCert/index.html')
@@ -10,4 +13,13 @@ def about(request):
     return render(request, 'PLCert/about.html')
 
 def courses(request):
-    return render(request, 'PLCert/courses.html')
+
+    courses =  Course.objects.all()
+    #variable set equal to Course from models to get all courses. 
+    #next pass course object into template using context
+    context = {
+        'title': 'Courses',
+        'courses':courses
+    }
+
+    return render(request, 'PLCert/courses.html', context)
