f729c57bbc682a9a3306e676c666911c9b24c40c
Chlece Neal <cneal2@luc.edu>
2018-02-18 15:26:48 -0600

    course page link does not work

--- PLI/PLCert/urls.py
+++ PLI/PLCert/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from django.conf.urls import url
 from . import views
 
-# ^ means the url will start with nothing and & means end with nothing 
+# ^ means the url will start with nothing and $ means end with nothing 
 # views.index  means looks in view.py file for a method called index and we named it index
 #? means parameter which is id and \d represents it should be a digit and + 
 urlpatterns = [
